fff8f1019f038ec754574b4633b2b65bd6c0bc67
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-10-07 14:42:26 +0200

    koniec zadania

--- lab01/skrypt.py
+++ lab01/skrypt.py
@@ -14,19 +14,22 @@ def display(args, show_index):
     print("Stop")
 
 
-def run(moves,move_descriptions):
-    result=[]
+def run(moves, move_descriptions):
+    result = []
     for x in moves:
         if x in move_descriptions:
             result.append(move_descriptions[x])
     return result
-        
 
 
+# display(sys.argv, False)
 
 
-#display(sys.argv, False)
+move_descp = {
+    "f": "Zwierzak idzie do przodu",
+    "b": "Zwierzak idzie do tyłu",
+    "l": "Zwierzak skręca w lewo",
+    "r": "Zwierzak skręca w prawo",
+}
 
-
-move_descp = {'f': "Zwierzak idzie do przodu", 'b': "Zwierzak idzie do tyłu", 'l': "Zwierzak skręca w lewo", 'r': "Zwierzak skręca w prawo"}
-print(run(sys.argv,move_descp))
+display(run(sys.argv, move_descp),False)
