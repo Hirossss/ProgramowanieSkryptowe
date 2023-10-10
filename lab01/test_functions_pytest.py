45b289f5366d37d17b077f0605895e255db7ecba
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-10-10 21:52:02 +0200

    funkcja testowa

--- lab01/test_functions_pytest.py
+++ lab01/test_functions_pytest.py
@@ -1,10 +1,9 @@
-import sys
 import pytest
 from skrypt import display, run
 
 
 @pytest.fixture
-def sample_moves():
+def sample_moves():                 # to musi byc na poczatku bo program wlacza wszystkie testy na raz i pozniej nie widzi tej def
     return ["f", "b", "l", "r"]
 
 
@@ -21,7 +20,6 @@ def test_display_with_index(capsys):
     captured = capsys.readouterr()
     assert captured.out == "Start\nargs[0] = arg1\nargs[1] = arg2\nStop\n"
 
-'''
 def test_run(sample_moves):
     move_descriptions = {
         "f": "Zwierzak idzie do przodu",
@@ -36,19 +34,7 @@ def test_run(sample_moves):
         "Zwierzak skręca w lewo",
         "Zwierzak skręca w prawo",
     ]
-'''
-def test_run(capsys):
-    move_descriptions = {
-        "f": "Zwierzak idzie do przodu",
-        "b": "Zwierzak idzie do tyłu",
-        "l": "Zwierzak skręca w lewo",
-        "r": "Zwierzak skręca w prawo",
-    }
-    run(sample_moves, move_descriptions)
-    captured = capsys.readouterr()
-    assert captured.out == [
-        "Zwierzak idzie do przodu",
-        "Zwierzak idzie do tyłu",
-        "Zwierzak skręca w lewo",
-        "Zwierzak skręca w prawo",
-    ]
+
+
+
+
