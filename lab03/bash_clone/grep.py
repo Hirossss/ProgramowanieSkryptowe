f192fb1cdf3069e90176ac2586cef65e0407e2b1
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-10-13 18:45:22 +0200

    finally

---
+++ lab03/bash_clone/grep.py
@@ -0,0 +1,26 @@
+import re
+
+def grep_function(i_option, w_option,word,text):
+    if not i_option and not w_option:
+        for line in text:
+            if word in line:
+                print(line)
+
+    elif i_option and not w_option:
+        for line in text:
+            if word.lower() in line.lower():
+                print(line)
+
+    elif w_option and not i_option:
+        word_pattern = r'\b{}\b'.format(re.escape(word))    # reg ex stack overflow
+        for line in text:
+            if re.search(word_pattern, line):
+                print(line)
+        
+    else:
+        for line in text:
+            word_pattern = r'\b{}\b'.format(re.escape(word))    # reg ex stack overflow
+        for line in text:
+            if re.search(word_pattern, line,re.IGNORECASE):
+                print(line)
+            
