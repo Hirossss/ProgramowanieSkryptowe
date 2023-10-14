abc9e73a991647ac118c503b7faefc7a14f952df
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-10-14 17:55:28 +0200

    a

--- lab03/bash_clone/skrypt2.py
+++ lab03/bash_clone/skrypt2.py
@@ -1,17 +1,11 @@
 import sys
 
 
-allowed_options_cut = ["d", "f", "i", "w"]
-allowed_options_grep = ["i", "w"]
-
-
 def skrypt2(args):
-    if args[0] == "cut":    # mozna to tez zrobic przy pomocy case
+    if args[0] == "cut":  # mozna to tez zrobic przy pomocy case
         from cut import cut_function
 
-        d_ind = args.index(
-            "-d"
-        )  # mozna to podac na sztywno jesli zakladamy ze bash dopuszcza jedna skladnie, ale wole dowolnosc  # noqa: E501
+        d_ind = args.index("-d")
         f_ind = args.index("-f")
         cut_function(args[d_ind + 1], args[f_ind + 1], args[5:])
     elif args[0] == "grep":
