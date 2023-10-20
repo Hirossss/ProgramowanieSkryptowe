e37cfdf3925e6a7a277b129490c7058b074011e1
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-10-20 08:31:57 +0200

    a

--- lab03/bash_clone/cut.py
+++
@@ -1,6 +0,0 @@
-def cut_function(delimiter, column, text):
-    column_int = int(column)
-    for line in text:
-        result = line.split(f"{delimiter}")
-        print(result[column_int - 1])
-    return 0
