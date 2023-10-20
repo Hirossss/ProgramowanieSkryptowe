81b25c1ef03301c9a340d3236e90ace701bdeabb
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-10-20 10:48:00 +0200

    a

---
+++ lab03/Vector2d.py
@@ -0,0 +1,52 @@
+class Vector2d:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def __str__(self):
+        return f"({self.__x},{self.__y})"
+
+    def __eq__(self, other):
+        if not isinstance(other, Vector2d):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.__x == other.__x and self.__x == other.__y
+
+    def add(self, other_Vector2d):
+        new_vector = Vector2d(
+            self.__x + other_Vector2d.__x, self.__y + other_Vector2d.__y
+        )
+        return new_vector
+
+    def subtract(self, other_Vector2d):
+        new_vector = Vector2d(
+            self.__x - other_Vector2d.__x, self.__y - other_Vector2d.__y
+        )
+        return new_vector
+
+    def precedes(self, other_Vector2d):
+        if self.__x <= other_Vector2d.__x and self.__y <= other_Vector2d.__y:
+            return True
+        return False
+
+    def follows(self, other_Vector2d):
+        if self.__x >= other_Vector2d.__x and self.__y >= other_Vector2d.__y:
+            return True
+        return False
+
+    def upperRight(self, other_Vector2d):
+        new_x = max(self.__x, other_Vector2d.__x)
+        new_y = max(self.__y, other_Vector2d.__y)
+        new_vector = Vector2d(new_x, new_y)
+        return new_vector
+
+    def lowerLeft(self, other_Vector2d):
+        new_x = min(self.__x, other_Vector2d.__x)
+        new_y = min(self.__y, other_Vector2d.__y)
+        new_vector = Vector2d(new_x, new_y)
+        return new_vector
+
+    def opposite(self):
+        new_vector = Vector2d(-self.__x, -self.__y)
+        return new_vector
