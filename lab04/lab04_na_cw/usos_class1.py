910a27838abee90f741f55cc74dc628e4a67c525
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-11-08 18:21:29 +0100

    a

--- lab04/lab04_na_cw/usos_class1.py
+++
@@ -1,6 +0,0 @@
-from Student import Student
-
-Jan_Kowalski = Student("Jan", "Kowalski")
-Anna_Kowalska = Student("Anna", "Kowalska")
-Joanna_Bielecka = Student("Joanna", "Bielecka")
-list_of_students = [Jan_Kowalski, Anna_Kowalska, Joanna_Bielecka]
