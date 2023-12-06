dfd7c85a43263558dfc0ad0a17556b32d00e7cf3
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-12-06 19:56:01 +0100

    a

--- lab8/cw/script-usos.js
+++ lab8/cw/script-usos.js
@@ -1,235 +1,73 @@
-let db;
+// Zdefiniuj kolekcję z danymi zahardkodowanymi
+const students = [
+    { id: 1, name: 'John', surname: 'Doe' },
+    // Dodaj więcej studentów według potrzeb
+];
 
-function openDatabase() {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open('USOSdata', 1);
+const subjects = [
+    { id: 1, name: 'Math', instructor: 'Prof. Smith' },
+    // Dodaj więcej przedmiotów według potrzeb
+];
 
-    request.onerror = function (event) {
-      console.error('Błąd otwarcia bazy danych:', event.target.errorCode);
-      reject('Error opening database');
-    };
+const grades = [
+    { studentId: 1, subjectId: 1, value: 4.5 },
+    // Dodaj więcej ocen według potrzeb
+];
 
-    request.onsuccess = function (event) {
-      console.log('Baza danych otwarta pomyślnie');
-      db = event.target.result;
-      resolve();
-    };
+// Funkcja do obsługi komendy
+function executeCommand() {
+    const commandInput = document.getElementById('cmdinput').value;
 
-    request.onupgradeneeded = function (event) {
-      db = event.target.result;
+    // Przykładowa obsługa komendy dodawania oceny
+    if (commandInput.startsWith('addGrade')) {
+        const params = commandInput.split(' ');
+        if (params.length === 4) {
+            const studentId = parseInt(params[1]);
+            const subjectId = parseInt(params[2]);
+            const value = parseFloat(params[3]);
 
-      const subjectsObjectStore = db.createObjectStore('subjects', { keyPath: 'name' });
-      const studentsObjectStore = db.createObjectStore('students', { keyPath: 'id', autoIncrement: true });
-      const gradesObjectStore = db.createObjectStore('grades', { keyPath: 'id', autoIncrement: true });
+            const studentExists = students.some(student => student.id === studentId);
+            const subjectExists = subjects.some(subject => subject.id === subjectId);
 
-      studentsObjectStore.createIndex('name', 'name', { unique: false });
-      gradesObjectStore.createIndex('subjectName', 'subjectName', { unique: false });
-      gradesObjectStore.createIndex('studentId', 'studentId', { unique: false });
-
-      const mathSubject = { name: 'Math', instructor: 'Dr. Smith' };
-      const physicsSubject = { name: 'Physics', instructor: 'Dr. Johnson' };
-      const computerScienceSubject = { name: 'Computer Science', instructor: 'Dr. Williams' };
-
-      subjectsObjectStore.add(mathSubject);
-      subjectsObjectStore.add(physicsSubject);
-      subjectsObjectStore.add(computerScienceSubject);
-    };
-  });
-}
-
-function addGrade(studentName, gradeValue, subjectName) {
-    openDatabase().then(() => {
-      const transaction = db.transaction(['students', 'subjects', 'grades'], 'readwrite');
-      const studentsStore = transaction.objectStore('students');
-      const subjectsStore = transaction.objectStore('subjects');
-      const gradesStore = transaction.objectStore('grades');
-  
-      const studentIndex = studentsStore.index('name');
-      const getStudent = studentIndex.get(studentName);
-  
-      getStudent.onsuccess = function (event) {
-        const student = event.target.result;
-  
-        if (student) {
-          // Student already exists, proceed to add the grade
-          const subjectRequest = subjectsStore.get(subjectName);
-  
-          subjectRequest.onsuccess = function (event) {
-            const subject = event.target.result;
-  
-            if (subject) {
-              const grade = {
-                studentId: student.id,
-                subjectName: subject.name,
-                value: gradeValue,
-              };
-  
-              const addGradeRequest = gradesStore.add(grade);
-  
-              addGradeRequest.onsuccess = function (event) {
-                console.log('Dodano ocenę do bazy danych:', event.target.result);
-              };
-  
-              addGradeRequest.onerror = function (event) {
-                console.error('Błąd dodawania oceny:', event.target.errorCode);
-              };
+            if (studentExists && subjectExists) {
+                grades.push({ studentId, subjectId, value });
+                console.log('Grade added successfully:', { studentId, subjectId, value });
+                display(); // Dodaj wywołanie funkcji display po dodaniu oceny
             } else {
-              console.error('Nie znaleziono przedmiotu o podanej nazwie.');
+                console.error('Student or subject not found!');
             }
-          };
-  
-          subjectRequest.onerror = function (event) {
-            console.error('Błąd pobierania przedmiotu:', event.target.errorCode);
-          };
         } else {
-          // Student doesn't exist, add the new student and then add the grade
-          const addStudentRequest = studentsStore.add({ name: studentName });
-  
-          addStudentRequest.onsuccess = function (event) {
-            console.log('Dodano nowego studenta do bazy danych:', event.target.result);
-            const studentId = event.target.result;
-  
-            const subjectRequest = subjectsStore.get(subjectName);
-  
-            subjectRequest.onsuccess = function (event) {
-              const subject = event.target.result;
-  
-              if (subject) {
-                const grade = {
-                  studentId: studentId,
-                  subjectName: subject.name,
-                  value: gradeValue,
-                };
-  
-                const addGradeRequest = gradesStore.add(grade);
-  
-                addGradeRequest.onsuccess = function (event) {
-                  console.log('Dodano ocenę do bazy danych:', event.target.result);
-                };
-  
-                addGradeRequest.onerror = function (event) {
-                  console.error('Błąd dodawania oceny:', event.target.errorCode);
-                };
-              } else {
-                console.error('Nie znaleziono przedmiotu o podanej nazwie.');
-              }
-            };
-  
-            subjectRequest.onerror = function (event) {
-              console.error('Błąd pobierania przedmiotu:', event.target.errorCode);
-            };
-          };
-  
-          addStudentRequest.onerror = function (event) {
-            console.error('Błąd dodawania nowego studenta:', event.target.errorCode);
-          };
+            console.error('Invalid command format for adding grade!');
         }
-      };
-  
-      getStudent.onerror = function (event) {
-        console.error('Błąd pobierania studenta:', event.target.errorCode);
-      };
-    }).catch((error) => {
-      console.error(error);
+    } else if (commandInput === 'display') {
+        display(); // Dodaj obsługę komendy 'display'
+    } else {
+        console.error('Unknown command!');
+    }
+}
+
+// Funkcja do wyświetlania danych
+function display() {
+    console.group('Grades');
+    
+    students.forEach(student => {
+        subjects.forEach(subject => {
+            const studentGrades = grades.filter(grade => grade.studentId === student.id && grade.subjectId === subject.id);
+
+            if (studentGrades.length > 0) {
+                const average = calculateAverage(studentGrades);
+                console.log(`Student: ${student.name} ${student.surname}, Subject: ${subject.name}, Instructor: ${subject.instructor}, Average: ${average}`);
+            } else {
+                console.warn(`No grades found for Student: ${student.name} ${student.surname}, Subject: ${subject.name}`);
+            }
+        });
     });
-}
-  
 
-function displayStudent(studentName) {
-  openDatabase().then(() => {
-    const transaction = db.transaction(['students', 'grades'], 'readonly');
-    const studentsStore = transaction.objectStore('students');
-    const gradesStore = transaction.objectStore('grades');
-
-    const studentIndex = studentsStore.index('name');
-    const studentRequest = studentIndex.get(studentName);
-
-    studentRequest.onsuccess = function (event) {
-      const student = event.target.result;
-
-      if (student) {
-        const gradesIndex = gradesStore.index('studentId');
-        const gradesRequest = gradesIndex.getAll(student.id);
-
-        gradesRequest.onsuccess = function (event) {
-          const studentGrades = event.target.result;
-          console.group('Oceny studenta', student.name);
-          studentGrades.forEach((grade) => {
-            console.log('Przedmiot:', grade.subjectName, 'Ocena:', grade.value);
-          });
-          console.groupEnd();
-        };
-      } else {
-        console.error('Nie znaleziono studenta o podanym imieniu.');
-      }
-    };
-
-    studentRequest.onerror = function (event) {
-      console.error('Błąd pobierania studenta:', event.target.errorCode);
-    };
-  }).catch((error) => {
-    console.error(error);
-  });
+    console.groupEnd();
 }
 
-
-function displaySubject(subjectName) {
-    openDatabase().then(() => {
-        const transaction = db.transaction(['subjects', 'grades', 'students'], 'readonly');
-        const subjectsStore = transaction.objectStore('subjects');
-        const gradesStore = transaction.objectStore('grades');
-        const studentsStore = transaction.objectStore('students');
-    
-        const subjectRequest = subjectsStore.get(subjectName);
-    
-        subjectRequest.onsuccess = function (event) {
-        const subject = event.target.result;
-    
-        if (subject) {
-            const studentsIndex = studentsStore.index('id');
-            const gradesIndex = gradesStore.index('subjectName');
-    
-            const studentsRequest = studentsIndex.getAll();
-            const gradesRequest = gradesIndex.getAll(subject.name);
-    
-            studentsRequest.onsuccess = function (event) {
-            const allStudents = event.target.result;
-            console.group('Oceny przedmiotu', subject.name, 'Prowadzący:', subject.instructor);
-            allStudents.forEach((student) => {
-                const studentGrades = gradesRequest.result.filter((grade) => grade.studentId === student.id);
-                console.group('Student:', student.name);
-                studentGrades.forEach((grade) => {
-                console.log('Ocena:', grade.value);
-                });
-                console.groupEnd();
-            });
-            console.groupEnd();
-            };
-        } else {
-            console.error('Nie znaleziono przedmiotu o podanej nazwie.');
-        }
-        };
-    });
+// Funkcja do obliczania średniej ocen
+function calculateAverage(grades) {
+    const totalValue = grades.reduce((sum, grade) => sum + grade.value, 0);
+    return totalValue / grades.length;
 }
-
-function clearDatabase() {
-    openDatabase().then(() => {
-      const transaction = db.transaction(['students', 'grades'], 'readwrite');
-      const studentsStore = transaction.objectStore('students');
-      const gradesStore = transaction.objectStore('grades');
-  
-      studentsStore.clear();
-      gradesStore.clear();
-  
-      console.log('Usunięto wszystkie dane związane ze studentami i ocenami. Dane przedmiotów pozostawione.');
-    }).catch((error) => {
-      console.error(error);
-    });
-}
-
-clearDatabase()
-addGrade('John Doe', 4.5, 'Math');
-addGrade('John Doe', 3.5, 'Physics');
-addGrade('Maria Kot', 4.5, 'Math');
-displayStudent('John Doe');
-displayStudent('Maria Kot');
