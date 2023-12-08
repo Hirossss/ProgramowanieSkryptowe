973ecba69edb63105f041a1d691c28809163725f
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-12-08 18:05:08 +0100

    l.

---
+++ lab9/dom/zad3/worker.js
@@ -0,0 +1,35 @@
+self.onmessage = function(event) {
+    var iterations = event.data;
+
+    self.onerror = function (error) {
+        console.error('Error in worker:', error.message);
+        self.postMessage({ error: error.message });
+        self.close();
+    };
+
+    var primes = [];
+    try {
+        for (var i = 0; i < iterations; i++) {
+            var candidate = i * (1000000000 * Math.random());
+            var isPrime = true;
+            for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+                if (candidate % c === 0) {
+                    // not prime
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primes.push(candidate);
+            }
+        }
+        self.postMessage(primes);
+    } catch (error) {   
+        // blad w obliczeniach
+        console.error('Error in worker:', error.message);
+        self.postMessage({ error: error.message });
+    } finally {
+        // zakonczenie watku, blok finally sie zawsze wykona niezaleznie od erroru lub jego jego braku
+        self.close();
+    }
+};
