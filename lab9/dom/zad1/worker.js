4c564fbc7f73c0c75ea9a0bb9d3c2636ee9e68cc
Nikodem <nikodemjakubowski.nj@gmail.com>
2023-12-12 22:28:14 +0100

    a

--- lab9/dom/zad1/worker.js
+++ lab9/dom/zad1/worker.js
@@ -27,8 +27,8 @@ self.onmessage = function(event) {
     } catch (error) {   
         // blad w obliczeniach
         console.error('Error in worker:', error.message);
-        window.alert('Error in worker: ' + error.message);
         //self.postMessage({ error: error.message });
+        window.alert('Error in worker: ' + error.message);
     } finally {
         // zakonczenie watku, blok finally sie zawsze wykona niezaleznie od erroru lub jego jego braku
         self.close();
